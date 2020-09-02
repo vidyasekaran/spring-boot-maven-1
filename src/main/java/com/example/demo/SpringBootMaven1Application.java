b4f76faaf83435074ff2cb47bc66405652e9ff2c
vidguru1008@gmail.com <@Oranges1974>
2020-09-02 17:37:02 +0530

    2nd commit

--- src/main/java/com/example/demo/SpringBootMaven1Application.java
+++ src/main/java/com/example/demo/SpringBootMaven1Application.java
@@ -8,6 +8,7 @@ public class SpringBootMaven1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMaven1Application.class, args);
+		System.out.println("Test");
 	}
 
 }
