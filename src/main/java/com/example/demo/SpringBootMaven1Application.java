7ff4ad526fca7f67375500779ceb5b9b8dd1677b
vidguru1008@gmail.com <@Oranges1974>
2020-09-04 12:14:15 +0530

    3rd push

--- src/main/java/com/example/demo/SpringBootMaven1Application.java
+++ src/main/java/com/example/demo/SpringBootMaven1Application.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootMaven1Application {
 
+	@GetMapping("/message")
+	public String getMessage() {
+		
+		return "Om guru";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMaven1Application.class, args);
 		System.out.println("Test");
